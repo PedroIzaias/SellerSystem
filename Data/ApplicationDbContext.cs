ff6cd79717f64389be5c2a0f3e820db83bb9fee0
Pedro Izaias B. Silva <88090206+PedroIzaias@users.noreply.github.com>
2023-08-17 19:19:57 +0000

    Other entities and second migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SelerSystem.Models;
 using SellerSystem.Models;
 
-    public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SellerSystem.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecords { get; set; }
     }
---
+++ Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+namespace SellerSystem.Models.Enums
+{
+    public enum SaleStatus
+    {
+        Pending,
+        Billed,
+        Canceled
+    }
+}
